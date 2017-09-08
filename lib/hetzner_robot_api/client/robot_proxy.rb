bc5c5a038bae4c3d7705f36b5ad50bf2bee53d61
Ervin Barta <ervin@renderedtext.com>
2017-09-08 10:24:15 +0200

    Add a proxy class to build up the URL for API calls

---
+++ lib/hetzner_robot_api/client/robot_proxy.rb
@@ -0,0 +1,25 @@
+require "byebug"
+
+module HetznerRobotApi
+  class Client
+    class RobotProxy
+      attr_reader :keys, :options
+
+      def initialize
+        @keys = []
+        @options = {}
+      end
+
+      def append(key, options = {})
+        # store HTTP query parameters
+        @options.merge!(options) if options
+
+        @keys << key
+      end
+
+      def url
+        @url = "#{ROBOT_URL}/" + @keys.join("/").gsub(/_/, '-')
+      end
+    end
+  end
+end
